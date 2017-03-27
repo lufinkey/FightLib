33a1c41ba899e0f7db79a95d22c9af97c579e8b4
lufinkey <luisfinke@gmail.com>
2017-03-26 21:33:27 -0400

    Collidable

---
+++ include/fightlib/entity/collision/Collidable.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include <fightlib/entity/draw/Sprite.hpp>
+#include "rects/CollisionRect.hpp"
+
+namespace fl
+{
+	typedef enum
+	{
+		COLLISIONSIDE_TOP,
+		COLLISIONSIDE_BOTTOM,
+		COLLISIONSIDE_LEFT,
+		COLLISIONSIDE_RIGHT
+	} CollisionSide;
+
+	class Collidable : public Sprite
+	{
+	public:
+		virtual bool isStaticCollisionBody() const = 0;
+		virtual const fgl::ArrayList<CollisionRect*>& getCollisionRects() const = 0;
+
+	protected:
+		virtual void shift(const fgl::Vector2d& offset) = 0;
+
+		virtual void onCollision(Collidable* entity, CollisionSide side) {}
+	};
+}
