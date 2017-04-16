05a5221d2f367d9744d303b99a56b594f66de028
lufinkey <luisfinke@gmail.com>
2017-04-16 11:09:28 -0400

    Drawable::getFlag

--- include/fightlib/entity/draw/Drawable.hpp
+++ include/fightlib/entity/draw/Drawable.hpp
@@ -9,7 +9,10 @@ namespace fl
 	{
 	public:
 		virtual ~Drawable() {}
+
 		virtual void update(const fgl::ApplicationData& appData) {}
 		virtual void draw(const fgl::ApplicationData& appData, fgl::Graphics graphics) const = 0;
+
+		virtual bool getFlag(const fgl::String& flag) const { return false; }
 	};
 }
