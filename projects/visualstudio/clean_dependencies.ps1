00f7fa6a638284470f44a2ff92a95aa3b1791bb4
lufinkey <luisfinke@gmail.com>
2017-06-18 12:06:01 -0400

    forgot to change the clean script

--- projects/visualstudio/clean_dependencies.ps1
+++ projects/visualstudio/clean_dependencies.ps1
@@ -1,19 +1,11 @@
 #!C:/WINDOWS/System32/WindowsPowerShell/v1.0/powershell -file
-param([string]$GameLibraryPath="")
+param([string]$FightLibPath="")
 
 $msbuild_path=(Resolve-Path HKLM:\SOFTWARE\MICROSOFT\MSBuild\ToolsVersions\14.0 | Get-ItemProperty -Name MSBuildToolsPath).MSBuildToolsPath
 $msbuild_path="$msbuild_path\msbuild.exe"
 
-echo "cleaning SDL"
-$SDL_path="$GameLibraryPath\external\SDL\SDL\VisualC"
-& "$msbuild_path" "$SDL_path\SDL.sln" /t:Clean
+echo "cleaning Box2D"
+$b2d_path="$FightLibPath\external\Box2D\Box2D\Build\vs2013"
+& "$msbuild_path" "$b2d_path\Box2D.sln" /t:Clean
 
-echo "cleaning SDL_image"
-$SDL_image_path="$GameLibraryPath\external\SDL\SDL_image\VisualC"
-& "$msbuild_path" "$SDL_image_path\SDL_image.sln" /t:Clean
-
-echo "cleaning SDL_ttf"
-$SDL_ttf_path="$GameLibraryPath\external\SDL\SDL_ttf\VisualC"
-& "$msbuild_path" "$SDL_ttf_path\SDL_ttf.sln" /t:Clean
-
-echo "Windows is a garbage OS" | Out-File "$GameLibraryPath\projects\visualstudio\dependency_clean.log"
+echo "Windows is a garbage OS" | Out-File "$FightLibPath\projects\visualstudio\dependency_clean.log"
